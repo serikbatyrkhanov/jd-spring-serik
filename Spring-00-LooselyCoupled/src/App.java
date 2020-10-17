a7f17024749f3516188d09f183ecf10261c3fc43
serikbatyrkhanov <Sara2017>
2020-10-17 11:54:06 -0400

    updates to App class

--- Spring-00-LooselyCoupled/src/App.java
+++ Spring-00-LooselyCoupled/src/App.java
@@ -1,12 +1,17 @@
 import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class App {
     public static void main(String[] args) {
         FullTimeMentor fullTimeMentor = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
 
         MentorAccount mentor = new MentorAccount(fullTimeMentor);
         mentor.manageAccount();
+
+        MentorAccount mentor2 = new MentorAccount(partTimeMentor);
+        mentor2.manageAccount();
     }
 }
