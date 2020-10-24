66f9cfde41049a4ca34b61d5f24ba5bddce105ff
serikbatyrkhanov <Sara2017>
2020-10-24 13:53:02 -0400

    added Java Annotations Value example

---
+++ Spring-05-JavaAnnotations-Value/src/main/java/com/serik/services/Java.java
@@ -0,0 +1,66 @@
+package com.serik.services;
+
+import com.serik.interfaces.Course;
+import com.serik.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+    @Value("JD1")
+    private String batch;
+    @Value("${instructor}")
+    private String instructor;
+    @Value("${days}")
+    private String[] days;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                '}';
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String[] getDays() {
+        return days;
+    }
+
+    public void setDays(String[] days) {
+        this.days = days;
+    }
+
+    //@Autowired
+    //@Qualifier("officeHours")
+    ExtraSessions extraSessions;
+
+    public Java(@Qualifier("officeHours") ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Java: " + (20 + extraSessions.getHours()));
+    }
+}
